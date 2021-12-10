f23477cf0108cebea2107767ce3b241835379592
Violeta Hernández <vi.hdz.p@gmail.com>
2021-12-09 23:21:47 -0600

    Wrote termination statement

--- src/prss.lean
+++ src/prss.lean
@@ -1,14 +1,16 @@
 import data.list.defs
 import tactic
 
+universe u
+
 namespace list
 
 /-- Splits a list right after the first element satisfying a predicate. -/
-def split_at_pred {α : Type*} (l : list α) (P : α → Prop) [decidable_pred P] : list α × list α :=
+def split_at_pred {α : Type u} (l : list α) (P : α → Prop) [decidable_pred P] : list α × list α :=
 l.split_at (l.find_index P).succ
 
 /-- `l.split' k` splits `l` right after the first element equal to `k`. -/
-def split_eq {α : Type*} (l : list α) (k : α) [decidable_eq α] : list α × list α :=
+def split_eq {α : Type u} (l : list α) (k : α) [decidable_eq α] : list α × list α :=
 l.split_at_pred (eq k)
 
 /-- Splits a list into the bad and good parts. -/
@@ -17,44 +19,80 @@ def split_prss : list ℕ → list ℕ × list ℕ
 | (0 :: l)       := ([], l)
 | ((n + 1) :: l) := l.split_eq n
 
+/-- Bad part of a list. -/
+abbreviation bad_part (l : list ℕ) : list ℕ := l.split_prss.fst
+
+/-- Good part of a list. -/
+abbreviation good_part (l : list ℕ) : list ℕ := l.split_prss.snd
+
+/-- The empty list splits into two copies of itself. -/
+theorem split_prss_nil : list.nil.split_prss = ([], []) := rfl
+
+/-- The bad and good parts together make the tail. -/
+theorem bad_append_good_eq_tail : ∀ l : list ℕ, l.bad_part ++ l.good_part = l.tail
+| []             := by refl
+| (0 :: l)       := by refl
+| ((n + 1) :: l) := begin
+    change (split_at _ _).fst ++ (split_at _ _).snd = _,
+    rw split_at_eq_take_drop _ l,
+    exact take_append_drop _ l
+  end
+
 /-- Appends a list to itself `n` times. -/
-def cycle {α : Type*} : list α → ℕ → list α
+def cycle {α : Type u} : list α → ℕ → list α
 | _ 0       := []
 | l (n + 1) := l ++ (l.cycle n)
 
-/-- The empty list cycled over and over is still the empty list. -/
-theorem cycle_nil {α : Type*} {n : ℕ} : ([] : list α).cycle n = [] :=
-begin
-  induction n with n hn,
-  { refl },
-  change ([] : list α).cycle n.succ with [] ++ [].cycle n,
-  rw hn,
-  refl
-end
+/-- Cycling a list zero times gives the empty list. -/
+theorem cycle_zero {α : Type u} (l : list α) : l.cycle 0 = [] := rfl
+
+/-- Cycling a list once gives itself. -/
+theorem cycle_one {α : Type u} (l : list α) : l.cycle 1 = l := l.append_nil
+
+/-- Cycling the empty list gives itself. -/
+theorem cycle_nil {α : Type u} : ∀ {n : ℕ}, ([] : list α).cycle n = []
+| 0       := by refl
+| (n + 1) := by { show nil ++ nil.cycle n = nil, by { rw cycle_nil, refl }}
 
 /-- `l.prss n` performs one step of PrSS expansion, and copies the bad part `n` times. -/
 def prss (l : list ℕ) (n : ℕ) : list ℕ :=
-let (b, g) := l.split_prss in (b.cycle n) ++ g
+l.bad_part.cycle n ++ l.good_part
 
 /-- Leading zeros just get removed. -/
-theorem prss_zero (l : list ℕ) {n : ℕ} : prss (0 :: l) n = l :=
+theorem prss_lead_zero (l : list ℕ) {n : ℕ} : (0 :: l).prss n = l :=
 begin
-  change (0 :: l).prss n with _ ++ _,
+  change (0 :: l).prss n with list.cycle [] n ++ _,
   rw cycle_nil,
   refl
+end
+
+/-- `l.prss 1` just gives the tail. -/
+theorem prss_one (l : list ℕ) : l.prss 1 = l.tail :=
+begin
+  change l.prss 1 with (list.cycle _ 1) ++ _,
+  rw cycle_one,
+  exact l.bad_append_good_eq_tail
 end
 
 /-- Standard PrSS lists. -/
 inductive is_standard : list ℕ → Prop
 | base (n : ℕ) : is_standard (list.range n)
-| tail (l : list ℕ) : is_standard l.tail
-| prss (n : ℕ) (l : list ℕ) : is_standard (l.prss n)
+| prss (l : list ℕ) (n : ℕ) : is_standard (l.prss n)
 
-/-- PrSS lists reachable from another. -/
-inductive reachable (l : list ℕ) : list ℕ → Prop
-| prss (n : ℕ) : reachable (l.prss n)
+/-- Tails of standard lists are standard. -/
+theorem tail_standard (l : list ℕ) : is_standard l.tail :=
+begin
+  convert is_standard.prss l 1,
+  exact l.prss_one.symm,
+end
 
-theorem termination {l : list ℕ} (hl : is_standard l) : l.reachable [] :=
+/-- Returns the function that applies `g 0`, `g 1`, ... `g (n - 1)` to `f`, starting from `a`. -/
+def apply {α β : Type u} (f : α → β → α) (a : α) (g : ℕ → β) : ℕ → α :=
+λ n, foldl f a ((list.range n).map g)
+
+/-- PrSS terminates. -/
+theorem termination {l : list ℕ} (hl : is_standard l) (g : ℕ → ℕ) :
+  ∃ n, apply list.prss l g n = [] :=
 sorry
 
 end list
